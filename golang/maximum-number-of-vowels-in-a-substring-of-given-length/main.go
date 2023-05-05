b8c57552942c4ca1205e00676fd8b039002eedd8
Sinitsin Ivan Vladimirovich <isinitsin@ozon.ru>
2023-05-05 19:26:02 +0400

    daily v2

--- golang/maximum-number-of-vowels-in-a-substring-of-given-length/main.go
+++ golang/maximum-number-of-vowels-in-a-substring-of-given-length/main.go
@@ -1,10 +1,20 @@
 package main
 
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/solutions/
+
 import "fmt"
 
-func isVowel(s string) int {
+const (
+	aSymbol = 97
+	eSymbol = 101
+	iSymbol = 105
+	oSymbol = 111
+	uSymbol = 117
+)
 
-	if s == "a" || s == "e" || s == "i" || s == "o" || s == "u" {
+func isVowel(s byte) int {
+
+	if s == aSymbol || s == eSymbol || s == iSymbol || s == oSymbol || s == uSymbol {
 		return 1
 	}
 
@@ -17,7 +27,7 @@ func maxVowels(s string, k int) int {
 
 	// init sliding windows
 	for i := 0 ; i < k && i < len(s) ; i += 1 {
-		vowelsCount += isVowel(string(s[i]))
+		vowelsCount += isVowel(s[i])
 	}
 
 	maxNumbersOfVowels := vowelsCount
@@ -26,8 +36,8 @@ func maxVowels(s string, k int) int {
 
 	for i := k ; i < len(s) ; i += 1 {
 
-		vowelsCount -= isVowel(string(s[i - k]))
-		vowelsCount += isVowel(string(s[i]))
+		vowelsCount -= isVowel(s[i - k])
+		vowelsCount += isVowel(s[i])
 
 		if vowelsCount > maxNumbersOfVowels {
 			maxNumbersOfVowels = vowelsCount
